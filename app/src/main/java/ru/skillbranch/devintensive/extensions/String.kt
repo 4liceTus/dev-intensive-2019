9f90150a9459cb5b1375bd6c9b80d5319dcd7ac9
Linael <alice.tus@mail.ru>
2020-04-13 19:30:40 +1000

    Add String.stripHtml()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,3 +4,17 @@ fun String.truncate(length: Int = 16): String {
     val trimString = this.trim()
     return if (trimString.length <= length) trimString else """${trimString.substring(0, length).trim()}..."""
 }
+
+fun String.stripHtml(): String{
+    /* пример escape-последовательности html: &amp; = & (ampersand)
+     . - любой символ
+     * - {0,} - 0 и более повторений символа
+     ? - ленивый квантификатор
+     | - или
+     [] - последовательность
+     ^ - отрицание
+     */
+    val htmlRegex = Regex("(<.*?>)|(&[a-z]{1,4}?;)")
+    val spaceRegex = Regex(" {2,}") // больше одного пробела
+    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
+}

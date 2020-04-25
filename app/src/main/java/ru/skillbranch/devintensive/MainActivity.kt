904f928df656b1785e6f12806e588b460d62efa0
Linael <alice.tus@mail.ru>
2020-04-25 15:12:07 +1000

    Fix Keyboard

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -13,6 +13,7 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.models.Bender
 import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.extensions.isKeyboardOpen
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,14 +43,15 @@ class MainActivity : AppCompatActivity() {
         textTxt.text = benderObj.askQuestion()
 
         sendBtn.setOnClickListener {
+            if(this.isKeyboardOpen()) {
+                this.hideKeyboard()
+            }
             sendAnswer()
-            //hideKeyboard()
         }
 
         messageEt.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                sendAnswer()
-                hideKeyboard()
+                sendBtn.performClick()
                 true
             }
             else false
--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,14 +1,14 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
-import android.content.Context
 import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard(){
     val focus = this.currentFocus
     focus?.let {
-        (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
+        (getSystemService(Activity.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
             it.hideSoftInputFromWindow(focus.windowToken, 0)
         }
     }
@@ -16,8 +16,9 @@ fun Activity.hideKeyboard(){
 
 fun Activity.isKeyboardOpen(): Boolean {
     val rect = Rect()
-    window.decorView.getWindowVisibleDisplayFrame(rect)
-    val heightRoot = window.decorView.rootView.height
+    val rootView = findViewById<View>(android.R.id.content)
+    rootView.getWindowVisibleDisplayFrame(rect)
+    val heightRoot = rootView.height
     val heightRect = rect.height()
     return (heightRoot - heightRect > 128)
 }

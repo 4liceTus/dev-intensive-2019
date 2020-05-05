b7712cbe20d64d3908c7967d52c201e6023f6ef4
Linael <alice.tus@mail.ru>
2020-05-05 21:08:32 +1000

    Init on 2h 18m lesson

---
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive
+
+import android.app.Application
+import android.content.Context
+
+class App: Application() {
+
+    companion object {
+        private var instance: App? = null
+
+        fun applicationContex(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
